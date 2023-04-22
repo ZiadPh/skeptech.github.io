b3444808ef7df6d71f172c99d98c86efce6a9e04
ZiadPh <62078458+ZiadPh@users.noreply.github.com>
2023-04-22 14:21:53 +0100

    Test 15

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/skeptech.githib.io",
+  base: "/skeptech.github.io/",
   plugins: [react()],
   resolve: {
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
